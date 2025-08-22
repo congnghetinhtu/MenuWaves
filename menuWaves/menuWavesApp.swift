e787679e48989c8b4d4ca8648e36b6ab692b9532
WhyNotSolar <sollinks.thnahsolar@gmail.com>
2025-08-23 05:35:04 +0700

    Initial Commit

---
+++ menuWaves/menuWavesApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuWavesApp.swift
+//  menuWaves
+//
+//  Created by Van Thanh Pham on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct menuWavesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ menuWavesTests/menuWavesTests.swift
@@ -0,0 +1,17 @@
+//
+//  menuWavesTests.swift
+//  menuWavesTests
+//
+//  Created by Van Thanh Pham on 8/23/25.
+//
+
+import Testing
+@testable import menuWaves
+
+struct menuWavesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
